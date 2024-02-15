ce8f5b07e3a92fbd5b4264d6bd898e4fe71e0a8b
Savallera <phenix-vn@yandex.ru>
2024-02-15 16:40:47 +0300

    first structure

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -5,7 +5,6 @@ import path from '../config/path.js';
 import plumber from 'gulp-plumber';
 import notify from 'gulp-notify';
 import newer from 'gulp-newer';
-import fonter from 'gulp-fonter';
 import ttf2woff2 from 'gulp-ttf2woff2';
 
 export default () => {
@@ -20,8 +19,6 @@ export default () => {
       })
     )
     .pipe(newer(path.fonts.dest))
-    .pipe(fonter({ formats: ['woff', 'ttf'] }))
-    .pipe(gulp.src(path.fonts.src + '.ttf'))
     .pipe(ttf2woff2())
     .pipe(gulp.dest(path.fonts.dest));
 };
