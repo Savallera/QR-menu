e2b412f4a698c951792767f66bc0b70373b3eec1
Savallera <phenix-vn@yandex.ru>
2024-02-27 19:31:55 +0300

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,18 @@
 
+const iconButtons = document.querySelectorAll('.icon-button');
+const buttons = document.querySelectorAll('.button');
+const scaleButton = (event) => {
+  event.classList.add('animation-scale');
+  setTimeout(() => {
+    event.classList.remove('animation-scale');
+  }, 350);
+};
+iconButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    scaleButton(button);
+  });
+});
+
 const cardLists = document.querySelectorAll('.page__card-deck');
 const cards = document.querySelectorAll('.card');
 
