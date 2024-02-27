b9de1964288ebaa90861a510777df0dd20b80170
Savallera <phenix-vn@yandex.ru>
2024-02-27 20:04:10 +0300

    404 page

--- src/blocks/animation-scale/animation-scale.js
+++ src/blocks/animation-scale/animation-scale.js
@@ -1,10 +1,9 @@
 const iconButtons = document.querySelectorAll('.icon-button');
-const buttons = document.querySelectorAll('.button');
 const scaleButton = (event) => {
   event.classList.add('animation-scale');
   setTimeout(() => {
     event.classList.remove('animation-scale');
-  }, 350);
+  }, 250);
 };
 iconButtons.forEach((button) => {
   button.addEventListener('click', () => {
